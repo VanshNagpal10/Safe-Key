dc9e902c64d5d286e8bdf7839d5262aad6ea2b1d
Vansh Nagpal <vanshnagpal@Vanshs-MacBook-Pro.local>
2024-07-26 13:40:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Navbar from './components/Navbar'
+import Manager from './components/Manager'
+import Footer from './components/Footer'
+
+function App() {
+
+  return (
+    <>
+      <div class="absolute top-0 z-[-2]  w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]">
+        <Navbar />
+        <Manager />
+        <Footer />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='bg-slate-800 text-white flex flex-col justify-center items-center  w-full  bottom-0'>
+            <div className="logo font-bold text-white text-2xl">
+                <span className='text-green-500'> &lt;</span>
+
+                <span>Safe</span><span className='text-green-500'>Key/&gt;</span>
+
+
+            </div>
+            <div className='flex justify-center items-center'> Created with <img className='w-6 mx-2' src="icons/heart.png" alt="" /> by Vansh </div>
+        </div>
+    )
+}
+
+export default Footer
